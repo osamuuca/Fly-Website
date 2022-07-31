e936e53725a86d57ccbc41c13ea1d279cec5a896
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-07-31 00:17:42 -0300

    feat: Criada a página inicial do site.

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+    return (
+        <>
+            <h1>FLY</h1>
+            <div>
+                <p>CODERS</p>
+                <p className="tec-parag">A TEC COMPANY</p>
+            </div>
+        </>
+    );
+}
+
+export default Hero;
