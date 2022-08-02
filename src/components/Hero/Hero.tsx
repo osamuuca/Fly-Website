a874e00e2af8b8269cec6bcb43f94128cd1c7b66
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-08-02 11:45:31 -0300

    fix/FLY-14: Corrigido problema do tamanho da seção

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,19 @@
 import "./Hero.css";
 import logo from "../../vendor/img/FlyLogo.png";
 import kanji from "../../vendor/img/Kanji.png";
-import { Container } from "react-bootstrap/";
 
 function Hero() {
     return (
         <section data-test="hero-section" className="hero-section">
-            <Container>
-                <div className="hero-logo">
-                    <img src={logo} alt="Logo" className="logo" />
-                </div>
-                <p className="hero-paragraph">A TEC COMPANY</p>
-                <div className="hero-kanji">
-                    <img src={kanji} alt="Logo" className="kanji" />
-                </div>
-            </Container>
+            <div className="hero-logo">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+            <p className="hero-paragraph">A TEC COMPANY</p>
+            <div className="hero-kanji">
+                <img src={kanji} alt="Logo" className="kanji" />
+            </div>
         </section>
     );
 }
-
 
 export default Hero;
