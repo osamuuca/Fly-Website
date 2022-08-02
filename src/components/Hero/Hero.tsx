99d75c16ea4845a817e260fcd82364d712f969e8
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-08-02 00:48:55 -0300

    fix: Retirado conteiner, mas será adicionado posteriormente.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,10 @@
 import "./Hero.css";
 import logo from "../../vendor/img/FlyLogo.png";
 import kanji from "../../vendor/img/Kanji.png";
-import { Container } from "react-bootstrap/";
 
 function Hero() {
     return (
         <section data-test="hero-section" className="hero-section">
-            <Container>
                 <div className="hero-logo">
                     <img src={logo} alt="Logo" className="logo" />
                 </div>
@@ -14,7 +12,6 @@ function Hero() {
                 <div className="hero-kanji">
                     <img src={kanji} alt="Logo" className="kanji" />
                 </div>
-            </Container>
         </section>
     );
 }
