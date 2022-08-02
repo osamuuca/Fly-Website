2f39fa5b019ef920bf5b272859ec992c37329443
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-08-02 01:08:51 -0300

    v1.0.2

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,12 @@
 import "./Hero.css";
 import logo from "../../vendor/img/FlyLogo.png";
 import kanji from "../../vendor/img/Kanji.png";
+import { Container } from "react-bootstrap/";
 
 function Hero() {
     return (
         <section data-test="hero-section" className="hero-section">
+            <Container>
                 <div className="hero-logo">
                     <img src={logo} alt="Logo" className="logo" />
                 </div>
@@ -12,6 +14,7 @@ function Hero() {
                 <div className="hero-kanji">
                     <img src={kanji} alt="Logo" className="kanji" />
                 </div>
+            </Container>
         </section>
     );
 }
