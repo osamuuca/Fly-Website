956cc2bfb84b49c2746026b27506a2c5d8c04ee1
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-08-03 15:34:03 -0300

    fix/FLY-7: Feitas alterações de estilo nos componentes

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,15 +1,21 @@
 import { Container } from "react-bootstrap/";
-import "./AboutUs.css"
+import "./AboutUs.css";
 
 function AboutUs() {
     return (
         <section className="about-us">
             <Container>
+                <div className="row">
+                    <div className="dash"></div>
                     <div className="about-us-info">
                         <h1 className="title">Now it’s time to fly coding</h1>
-                        <p className="paragraph">We create competitive advantage through accelerated technology innovation. We provide the tools, talent, and processes needed to accelerate your path to market leadership.</p>
+                        <p className="paragraph">
+                            We create competitive advantage through accelerated technology innovation. We provide the tools, talent, and processes
+                            needed to accelerate your path to market leadership.
+                        </p>
                         <button className="buttom">tell me more</button>
                     </div>
+                </div>
             </Container>
         </section>
     );
