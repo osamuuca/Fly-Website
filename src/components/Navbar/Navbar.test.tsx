99d2f41f8635c4222bb2e51f9aa74258b3228e77
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-08-02 22:10:00 -0300

    fix/FLY-7: Corrigidos testes da Navbar

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -5,21 +5,21 @@ describe("Verificaç~ao do conteúdo da navbar", () => {
     test("Verificar se o navlink está exibindo o nome corretamente", () => {
         render(<NavBar />);
 
-        const Navlink = screen.getByText("ABOUT US");
+        const Navlink = screen.getByText("About Us");
         expect(Navlink).toBeInTheDocument();
     });
 
     test("Verificar se o navlink está exibindo o nome corretamente", () => {
         render(<NavBar />);
 
-        const Navlink = screen.getByText("PROJECTS");
+        const Navlink = screen.getByText("Projects");
         expect(Navlink).toBeInTheDocument();
     });
     
     test("Verificar se o navlink está exibindo o nome corretamente", () => {
         render(<NavBar />);
 
-        const Navlink = screen.getByText("OUR TEAM");
+        const Navlink = screen.getByText("Our Team");
         expect(Navlink).toBeInTheDocument();
     });
 
